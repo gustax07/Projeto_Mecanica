a228297c9b845979088bfb94c1416f9f504a783b
Rafael teixeira <Teixeiraflavia842@gmail.com>
2025-08-12 16:09:51 -0300

    Tela Principal

---
+++ mecanica/TelaPrincipal.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace mecanica
+{
+    partial class TelaPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pibTelaPrincipal = new PictureBox();
+            lblTelaPrincipal = new Label();
+            btnAdicionarservico = new Button();
+            pictureBox2 = new PictureBox();
+            btnComprarpeca = new Button();
+            btnHistoricoPedidos = new Button();
+            btnHistoricoCompras = new Button();
+            btnCadastrarmecanicos = new Button();
+            btnSimularPintura = new Button();
+            ((System.ComponentModel.ISupportInitialize)pibTelaPrincipal).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // pibTelaPrincipal
+            // 
+            pibTelaPrincipal.Image = Properties.Resources._2370744_tecnologia_fundo_com_um_conceito_mecanico_vetor;
+            pibTelaPrincipal.Location = new Point(-10, -4);
+            pibTelaPrincipal.Name = "pibTelaPrincipal";
+            pibTelaPrincipal.Size = new Size(820, 397);
+            pibTelaPrincipal.SizeMode = PictureBoxSizeMode.Zoom;
+            pibTelaPrincipal.TabIndex = 0;
+            pibTelaPrincipal.TabStop = false;
+            // 
+            // lblTelaPrincipal
+            // 
+            lblTelaPrincipal.AutoSize = true;
+            lblTelaPrincipal.BackColor = Color.Black;
+            lblTelaPrincipal.Font = new Font("Arial Rounded MT Bold", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblTelaPrincipal.ForeColor = Color.White;
+            lblTelaPrincipal.Image = Properties.Resources._2370744_tecnologia_fundo_com_um_conceito_mecanico_vetor1;
+            lblTelaPrincipal.Location = new Point(278, 9);
+            lblTelaPrincipal.Name = "lblTelaPrincipal";
+            lblTelaPrincipal.Size = new Size(238, 37);
+            lblTelaPrincipal.TabIndex = 1;
+            lblTelaPrincipal.Text = "Tela principal ";
+            // 
+            // btnAdicionarservico
+            // 
+            btnAdicionarservico.BackColor = Color.Black;
+            btnAdicionarservico.Font = new Font("Arial Rounded MT Bold", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnAdicionarservico.ForeColor = Color.White;
+            btnAdicionarservico.Image = Properties.Resources._2370744_tecnologia_fundo_com_um_conceito_mecanico_vetor;
+            btnAdicionarservico.Location = new Point(509, 270);
+            btnAdicionarservico.Name = "btnAdicionarservico";
+            btnAdicionarservico.Size = new Size(216, 58);
+            btnAdicionarservico.TabIndex = 2;
+            btnAdicionarservico.Text = "Adicionar Serviços";
+            btnAdicionarservico.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = SystemColors.WindowText;
+            pictureBox2.Image = Properties.Resources.mechanic__1_;
+            pictureBox2.Location = new Point(12, 85);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(186, 243);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
+            // 
+            // btnComprarpeca
+            // 
+            btnComprarpeca.BackColor = Color.Black;
+            btnComprarpeca.Font = new Font("Arial Rounded MT Bold", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnComprarpeca.ForeColor = Color.White;
+            btnComprarpeca.Image = Properties.Resources._2370744_tecnologia_fundo_com_um_conceito_mecanico_vetor2;
+            btnComprarpeca.Location = new Point(262, 270);
+            btnComprarpeca.Name = "btnComprarpeca";
+            btnComprarpeca.Size = new Size(216, 58);
+            btnComprarpeca.TabIndex = 4;
+            btnComprarpeca.Text = "Comprar Peças";
+            btnComprarpeca.UseVisualStyleBackColor = false;
+            // 
+            // btnHistoricoPedidos
+            // 
+            btnHistoricoPedidos.BackColor = Color.Black;
+            btnHistoricoPedidos.Font = new Font("Arial Rounded MT Bold", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnHistoricoPedidos.ForeColor = Color.White;
+            btnHistoricoPedidos.Image = Properties.Resources._2370744_tecnologia_fundo_com_um_conceito_mecanico_vetor1;
+            btnHistoricoPedidos.Location = new Point(509, 177);
+            btnHistoricoPedidos.Name = "btnHistoricoPedidos";
+            btnHistoricoPedidos.Size = new Size(216, 61);
+            btnHistoricoPedidos.TabIndex = 5;
+            btnHistoricoPedidos.Text = "Histórico de Pedidos";
+            btnHistoricoPedidos.UseVisualStyleBackColor = false;
+            // 
+            // btnHistoricoCompras
+            // 
+            btnHistoricoCompras.BackColor = Color.Black;
+            btnHistoricoCompras.Font = new Font("Arial Rounded MT Bold", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnHistoricoCompras.ForeColor = Color.White;
+            btnHistoricoCompras.Image = Properties.Resources._2370744_tecnologia_fundo_com_um_conceito_mecanico_vetor2;
+            btnHistoricoCompras.Location = new Point(262, 177);
+            btnHistoricoCompras.Name = "btnHistoricoCompras";
+            btnHistoricoCompras.Size = new Size(216, 61);
+            btnHistoricoCompras.TabIndex = 6;
+            btnHistoricoCompras.Text = "Histórico de Compras ";
+            btnHistoricoCompras.UseVisualStyleBackColor = false;
+            // 
+            // btnCadastrarmecanicos
+            // 
+            btnCadastrarmecanicos.BackColor = Color.Black;
+            btnCadastrarmecanicos.Font = new Font("Arial Rounded MT Bold", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnCadastrarmecanicos.ForeColor = Color.White;
+            btnCadastrarmecanicos.Image = Properties.Resources._2370744_tecnologia_fundo_com_um_conceito_mecanico_vetor;
+            btnCadastrarmecanicos.Location = new Point(509, 85);
+            btnCadastrarmecanicos.Name = "btnCadastrarmecanicos";
+            btnCadastrarmecanicos.Size = new Size(216, 61);
+            btnCadastrarmecanicos.TabIndex = 7;
+            btnCadastrarmecanicos.Text = "Cadastrar Mecânicos";
+            btnCadastrarmecanicos.UseVisualStyleBackColor = false;
+            // 
+            // btnSimularPintura
+            // 
+            btnSimularPintura.BackColor = Color.Black;
+            btnSimularPintura.Font = new Font("Arial Rounded MT Bold", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnSimularPintura.ForeColor = Color.White;
+            btnSimularPintura.Image = Properties.Resources._2370744_tecnologia_fundo_com_um_conceito_mecanico_vetor2;
+            btnSimularPintura.Location = new Point(262, 85);
+            btnSimularPintura.Name = "btnSimularPintura";
+            btnSimularPintura.Size = new Size(216, 61);
+            btnSimularPintura.TabIndex = 8;
+            btnSimularPintura.Text = "Simular Pinturas";
+            btnSimularPintura.UseVisualStyleBackColor = false;
+            // 
+            // TelaPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(797, 393);
+            Controls.Add(btnSimularPintura);
+            Controls.Add(btnCadastrarmecanicos);
+            Controls.Add(btnHistoricoCompras);
+            Controls.Add(btnHistoricoPedidos);
+            Controls.Add(btnComprarpeca);
+            Controls.Add(pictureBox2);
+            Controls.Add(btnAdicionarservico);
+            Controls.Add(lblTelaPrincipal);
+            Controls.Add(pibTelaPrincipal);
+            Name = "TelaPrincipal";
+            Text = "TelaPrincipal";
+            ((System.ComponentModel.ISupportInitialize)pibTelaPrincipal).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pibTelaPrincipal;
+        private Label lblTelaPrincipal;
+        private Button btnAdicionarservico;
+        private PictureBox pictureBox2;
+        private Button btnComprarpeca;
+        private Button btnHistoricoPedidos;
+        private Button btnHistoricoCompras;
+        private Button btnCadastrarmecanicos;
+        private Button btnSimularPintura;
+    }
+}
