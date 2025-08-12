80872d1201c4a0dbad481de931dd5321c98761a1
RAFAEL HENRICK VIEIRA DOS SANTOS <rafael.hvsantos1@senacsp.edu.br>
2025-08-12 15:29:46 -0300

    adicionei a classe Orcamento

---
+++ mecanica/Model/Orcamento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mecanica.Model
+{
+    internal class Orcamento
+    {
+        public uint Id { get; set; }
+        public DateTime Data_prevista { get; set; }
+        public string Forma_pagamento { get; set; }
+        public double Valor_estimado { get; set; }
+        public string Descricao_problema { get; set; }
+    }
+}
